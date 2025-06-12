da651e5da8472fe6e34170359b810583276cdcd5
nakulmahajan.ac.in@gmail.com <nakulmahajan.ac.in@gmail.com>
2025-06-12 10:20:34 +0530

    Added a custom button for the obstacle with the indexing 1-based

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let grid = [];
       let rows = 3,
         cols = 3;
-      let black_row = 1,
-        black_col = 1;
+     let black_row = -1 ;
+     let black_col =-1 ;
       let delay = 1000;
       let totalPaths = 0;
 
@@ -10,7 +10,7 @@ let grid = [];
         return new Promise((resolve) => setTimeout(resolve, ms));
       }
 
-      function createGrid(r, c) {
+      function createGrid(r, c , black_row , black_col) {
         const container = document.getElementById("gridContainer");
         container.innerHTML = "";
         container.style.gridTemplateColumns = `repeat(${c}, 40px)`;
@@ -32,8 +32,9 @@ let grid = [];
         }
       }
 
-      async function animatePaths(r, c, i = 0, j = 0, path = []) {
-        if (i >= r || j >= c) return 0; //....
+      async function animatePaths(r, c, i , j , path = [] , black_row , black_col) {
+        if (i >= r || j >= c) return 0; 
+
         if (i == black_row && j == black_col) return 0;
         path.push([i, j]);
         grid[i][j].classList.add("path");
@@ -46,8 +47,8 @@ let grid = [];
           ).innerText = `Paths found: ${totalPaths}`;
           await sleep(1000);
         } else {
-          await animatePaths(r, c, i + 1, j, path);
-          await animatePaths(r, c, i, j + 1, path);
+          await animatePaths(r, c, i + 1, j, path ,  black_row , black_col);
+          await animatePaths(r, c, i, j + 1, path , black_row , black_col);
         }
 
         // Backtrack
@@ -60,10 +61,12 @@ let grid = [];
       async function startAnimation() {
         rows = parseInt(document.getElementById("rows").value);
         cols = parseInt(document.getElementById("cols").value);
+        black_row = parseInt(document.getElementById("black_rows_id").value) - 1;
+        black_col = parseInt(document.getElementById("black_cols_id").value) - 1;
         totalPaths = 0;
         document.getElementById("result").innerText = "Animating...";
-        createGrid(rows, cols);
-        await animatePaths(rows, cols);
+        createGrid(rows, cols , black_row , black_col);
+        await animatePaths(rows, cols ,0, 0,[] , black_row , black_col);
         document.getElementById("result").innerText += ` — Animation complete.`;
       }
 
